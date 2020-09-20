584ffedcb52ec7c2e785a10d1403a98c857a7dc1
Shamseer Ali <shamzi.erz@gmail.com>
2020-09-20 23:20:19 +0530

    Chat Image section

---
+++ Hunter/Cells/TableviewCells/HunterChatSenderImageCell.swift
@@ -0,0 +1,26 @@
+//
+//  HunterChatSenderImageCell.swift
+//  Hunter
+//
+//  Created by Shamseer on 20/09/20.
+//  Copyright © 2020 Zubin Manak. All rights reserved.
+//
+
+import UIKit
+
+class HunterChatSenderImageCell: UITableViewCell {
+
+    @IBOutlet weak var imgSentImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
